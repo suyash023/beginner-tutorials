2ec3d063fb9a6064ac9e295257540e85ac15e7d1
Suyash Yeotikar <suyashyeotikar@gmail.com>
2019-10-26 17:51:25 -0400

    Create catkin workspace with source. Create catkin package beginner_turtorials within src. Added depndencies of rospy, roscpp and std_msgs. built directory and sourced it via setup.bash. Edited packages.xml file. Checked dependencies using rospack.

---
+++ catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/suyash/Desktop/software_dev_for_robotics/Week8/beginner_tutorials/catkin_ws/src/beginner_tutorials
+# Build directory: /home/suyash/Desktop/software_dev_for_robotics/Week8/beginner_tutorials/catkin_ws/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
