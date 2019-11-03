122c46eb73994b04968c77d19f36a5f944c45a4d
Suyash Yeotikar <suyashyeotikar@gmail.com>
2019-11-03 13:31:18 -0500

    Changed ROS_INFO to stream in chatter callback

--- src/listener.cpp
+++ src/listener.cpp
@@ -21,7 +21,7 @@
  * @return none
  */
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO_STREAM("I heard: [%s]" << msg->data.c_str());
 }
 
 /**
